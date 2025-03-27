e994cb7645a890f956d705655e101f4c8a3af854
Dziudita <Stakesheep.team@gmail.com>
2025-03-27 14:22:35 +0200

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main style={{ textAlign: "center", paddingTop: "50px" }}>
+      <h1>StakeSheep Mirror is coming soon...</h1>
+    </main>
+  );
+}
